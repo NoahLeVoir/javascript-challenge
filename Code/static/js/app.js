aac8494a8193d06fcabaf953a2618f64b02b87ac
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-04-16 14:06:14 -0500

    Setting up layout for app.js script to build table

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// D3 to recognize the selection of the table
+let table_body = d3.select("table_body")
+
+// Functions
+
+    // Building the table
+
+
+    // Button click
+
+
+// Calling the functions to build the table on the click
